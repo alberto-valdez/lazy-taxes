69239118ccdcdbddf021c1e929612a8c0c6d16fe
¨albertovaldez-clivi¨ <carlos.valdez@clivi.com.mx>
2025-04-02 00:03:09 -0600

    add create user handler

--- internal/handlers/user_handler.go
+++ internal/handlers/user_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/alberto-valdez/lazy-taxes/internal/models"
 	"github.com/alberto-valdez/lazy-taxes/internal/services"
 	"github.com/gin-gonic/gin"
 )
@@ -20,12 +21,27 @@ func NewUserHandler(userService services.UserService) *UserHanlder {
 func (h *UserHanlder) RegisterRoutes(router *gin.RouterGroup) {
 	users := router.Group("/users")
 	{
+		users.POST("/create", h.handleCreateUser)
 		users.GET("/init", h.handleGetUser)
 	}
 }
 
 func (h *UserHanlder) handleGetUser(c *gin.Context) {
 	data, err := h.userService.GetUser()
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": data})
+}
+
+func (h *UserHanlder) handleCreateUser(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	data, err := h.userService.CreateUser(user)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": err})
 		return
